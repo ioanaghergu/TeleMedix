5680554f39e7b95af36ad57d4a6be915f0d71b08
Bianca <bianca-filofteia.andrei@s.unibuc.ro>
2024-12-14 04:12:12 +0200

    Refactored doctors list and consultation form

--- website/consultations.py
+++ website/consultations.py
@@ -1,7 +1,5 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
-from flask_login import current_user, login_required
-from .models import Doctor, Specialization, Appointment
-from . import db
+from flask import Blueprint, render_template, request, flash, redirect, url_for, current_app
+from flask_login import login_required, current_user
 from datetime import datetime
 
 consultation = Blueprint('consultation', __name__)
@@ -10,45 +8,43 @@ consultation = Blueprint('consultation', __name__)
 @login_required
 def add_consultation():
     if request.method == 'POST':
-        patient_id = current_user.id
-        specialization_id = request.form.get('specialization')
-        doctor_id = request.form.get('doctor')
-        date = request.form.get('date')
-        time = request.form.get('time')
+        specializationId = request.form.get('specialization')
+        doctorId = request.form.get('doctor')
+        appointment_datetime = request.form.get('appointmentDatetime')
         notes = request.form.get('notes')
 
-        if not specialization_id or not doctor_id or not date:
-            flash("All fields are required", category="error")
-            return redirect(url_for('consultation.add_consultation'))
+        # Convert appointment_datetime to a datetime object
         try:
-            appointment_date = datetime.strptime(f"{date} {time}", '%Y-%m-%d %H:%M')
+            appointment_datetime = datetime.strptime(appointment_datetime, '%Y-%m-%dT%H:%M')
         except ValueError:
-            flash("Invalid date format", category="error")
+            flash("Invalid date and time format", category="error")
             return redirect(url_for('consultation.add_consultation'))
 
-        if appointment_date < datetime.now():
+        if appointment_datetime < datetime.now():
             flash("The date must be in the future", category="error")
             return redirect(url_for('consultation.add_consultation'))
 
-        existing_appointment = Appointment.query.filter_by(doctor_id=doctor_id, date=appointment_date).first()
+        conn = current_app.db
+        cursor = conn.cursor()
+
+        existing_appointment = cursor.execute("SELECT * FROM [Appointment] WHERE [medicID] = ? AND [appointment_date] = ?", doctorId, appointment_datetime).fetchone()
         if existing_appointment:
             flash("There is already an appointment with this doctor at the same date and time", category="error")
             return redirect(url_for('consultation.add_consultation'))
 
-        newAppointment = Appointment(patient_id=patient_id, doctor_id=doctor_id, date=appointment_date, notes=notes)
-        db.session.add(newAppointment)
-        db.session.commit()
+        cursor.execute("INSERT INTO [Appointment] ([pacientID], [medicID], [specializationID], [appointment_date], [notes], [serviceID]) VALUES (?, ?, ?, ?, ?, ?)", current_user.userid, doctorId, specializationId, appointment_datetime, notes, 1)
+        conn.commit()
+
         flash("Your consultation form was completed successfully", category="success")
         return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html", user=current_user)
-
+    specializations = current_app.db.cursor().execute("SELECT * FROM [Specialization]").fetchall()
+    return render_template("consultation_form.html", user=current_user, specializations=specializations)
 
 @consultation.route('/attended-consultations', methods=['GET'])
 @login_required
 def get_consultations():
-    appointments = Appointment.query.filter_by(patient_id=current_user.id).all()
-    print(appointments)
-    return render_template("attended_consultations.html", appointments=appointments, user=current_user)
-
-
+    conn = current_app.db
+    cursor = conn.cursor()
+    appointments = cursor.execute("SELECT [appointment_date], [notes], [username], [specialization_name] FROM [Appointment], [User], [Medic], [Specialization] WHERE [Appointment].[medicID] = [Medic].[medicID] AND [Medic].[medicID] = [User].[userID] AND [Medic].[specializationID] = [Specialization].[specializationID] AND [Appointment].[pacientID] = ?", current_user.userid).fetchall()
+    return render_template("consultations/attended_consultations.html", appointments=appointments)
